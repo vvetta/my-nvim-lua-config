72de1f53ed5a9c170152c2455209e1071adff3cc
Nikita <drpsih2004@gmail.com>
2024-08-28 17:08:24 +0300

    add vimplug and some plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require('base/search')
 require('base/tabs')
 require('base/other')
+require('plugins/vim-plug-init')
---
+++ lua/plugins/vim-plug-init.lua
@@ -0,0 +1,17 @@
+vim.cmd [[
+call plug#begin('~/.config/nvim/plugged')
+
+Plug 'junegunn/vim-plug'
+Plug 'mhinz/vim-startify'
+Plug 'airblade/vim-gitgutter'
+Plug 'preservim/nerdtree'
+Plug 'ryanoasis/vim-devicons'
+Plug 'Xuyuanp/nerdtree-git-plugin'
+Plug 'arcticicestudio/nord-vim'
+Plug 'itchyny/lightline.vim'
+Plug 'tpope/vim-fugitive'
+Plug 'dstein64/nvim-scrollview'
+
+call plug#end()
+]]
+
